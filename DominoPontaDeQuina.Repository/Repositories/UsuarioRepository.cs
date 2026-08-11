99bb118c0b768836de74268f3b34f8f848012942
profvinicius84 <profvinicius.costa@fiap.com.br>
2026-08-11 00:15:02 -0300

    deixa repositorio como exercicio

--- DominoPontaDeQuina.Repository/Repositories/UsuarioRepository.cs
+++ DominoPontaDeQuina.Repository/Repositories/UsuarioRepository.cs
@@ -1,19 +1,5 @@
-using DominoPontaDeQuina.Domain.Entities;
-using DominoPontaDeQuina.Repository.Context;
-using Microsoft.EntityFrameworkCore;
-
 namespace DominoPontaDeQuina.Repository.Repositories;
 
-public class UsuarioRepository(DominoDbContext contexto)
+public class UsuarioRepository
 {
-    public async Task<Usuario> AdicionarAsync(Usuario usuario, CancellationToken cancelamento = default)
-    {
-        ArgumentNullException.ThrowIfNull(usuario);
-        await contexto.Set<Usuario>().AddAsync(usuario, cancelamento);
-        await contexto.SaveChangesAsync(cancelamento);
-        return usuario;
-    }
-
-    public Task<Usuario?> ObterPorEmailAsync(string email, CancellationToken cancelamento = default) =>
-        contexto.Set<Usuario>().SingleOrDefaultAsync(usuario => usuario.Email == email, cancelamento);
 }
